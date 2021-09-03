279901c92b935acd6de2014f9e0191bc6130883a
Godlove <ntangakalegodlove@gmail.com>
2021-09-03 21:32:06 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+let Prof = document.getElementById("pp");
+let Profil = document.getElementById("profil");
+
+Prof.onclick = function () {
+	Profil.style.display = "block";
+}
+
+	Profil.onmouseleave = function () {
+		Profil.style.display = "none";
+	}
